d05fa1c7f22f077c232d47bdd0eb90ff9a1c7f3c
Tejas Kothari <tejas23682320@gmail.com>
2023-05-17 18:37:51 +0800

    cron job

---
+++ src/pages/api/cron.ts
@@ -0,0 +1,88 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
+import { rpcEndpoint } from "@/config/sei";
+import {
+  SenseifiStakingNllClient,
+  SenseifiStakingNllQueryClient,
+} from "@/contract_clients/SenseifiStakingNll.client";
+import {
+  DirectSecp256k1HdWallet,
+  OfflineDirectSigner,
+} from "@cosmjs/proto-signing";
+import { seiStakingNLLContract } from "@/config/contracts";
+import { StdFee, coin } from "@cosmjs/amino";
+import { nsToSecs } from "@/utils";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  try {
+    if (process.env.ADMIN_MNEMONIC === undefined)
+      throw Error("Mnemonic of admin wallet not found");
+
+    const signer = await DirectSecp256k1HdWallet.fromMnemonic(
+      process.env.ADMIN_MNEMONIC,
+      {
+        prefix: "SEI",
+      }
+    );
+
+    const adminAddr = await signer.getAccounts().then((v) => v[0].address);
+
+    const client = await SigningCosmWasmClient.connectWithSigner(
+      rpcEndpoint,
+      signer
+    );
+
+    const readContract = new SenseifiStakingNllQueryClient(
+      client,
+      seiStakingNLLContract
+    );
+
+    const params = await readContract.getParams();
+    const globalState = await readContract.getGlobalState();
+
+    if (
+      Math.floor(Date.now() / 1000) - nsToSecs(globalState.last_unbond_time) <=
+      Number(params.min_unbond_interval_sec)
+    ) {
+      return response
+        .status(200)
+        .json({ success: true, message: "Not the time to unbond" });
+    }
+
+    if (BigInt(globalState.total_to_be_unbonded) === BigInt(0)) {
+      return response
+        .status(200)
+        .json({ success: true, message: "0 tokens to be unbonded" });
+    }
+
+    const writeContract = new SenseifiStakingNllClient(
+      client,
+      adminAddr,
+      seiStakingNLLContract
+    );
+
+    const fee: StdFee = {
+      amount: [coin("10000", params.denom)],
+      gas: "500000",
+    };
+
+    const result = await writeContract.unbond(fee);
+
+    return response
+      .status(200)
+      .json({ success: true, txHash: result.transactionHash });
+  } catch (e) {
+    let errorMsg = "";
+
+    if (typeof e === "string") {
+      errorMsg = e.toUpperCase();
+    } else if (e instanceof Error) {
+      errorMsg = e.message;
+    }
+
+    return response.status(500).json({ success: false, error: errorMsg });
+  }
+}
