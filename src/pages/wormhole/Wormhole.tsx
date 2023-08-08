40ba1ba0f2c383eb62f2cd2a805768ce0f5825ce
npanium <npanium@gmail.com>
2023-08-08 16:53:44 +1000

    page title edit

--- src/pages/wormhole/Wormhole.tsx
+++ src/pages/wormhole/Wormhole.tsx
@@ -45,7 +45,7 @@ const Wormhole = () => {
     <>
       <Box>
         <Head>
-          <title>Sensei Portfolio page</title>
+          <title>Sensei Wormhole bridge</title>
           <meta name="description" content="Your portfolio on SenSei Fi" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
